640a58df3af500ffdddac089b3600de78c0d7b36
nakanuma <k023g0121@g.neec.ac.jp>
2024-06-27 18:19:06 +0900

    プッシュ

--- Matrix.h
+++ Matrix.h
@@ -20,7 +20,7 @@ public:
 		float m20, float m21, float m22, float m23,
 		float m30, float m31, float m32, float m33);
 	
-	Matrix operator + (const Matrix& m1)const;
+	Matrix operator + (const Matrix& m1)const; 
 
 	Matrix operator+=(const Matrix& m1);
 
